4a12bfeedd84f33eae5a62493f0866ea3e511ffc
ednezer <godoirezende@gmail.com>
2019-10-23 22:15:07 -0300

    Colocando ion-item no mensagem listar

--- src/pages/mensagem-listar/mensagem-listar.ts
+++ src/pages/mensagem-listar/mensagem-listar.ts
@@ -1,36 +1,81 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController,
+  LoadingController
+} from 'ionic-angular';
 import { MensagemService } from '../../services/domain/mensagem.service';
 import { MensagemDTO } from '../../models/mensagem.dto';
-
 
 @IonicPage()
 @Component({
   selector: 'page-mensagem-listar',
-  templateUrl: 'mensagem-listar.html',
+  templateUrl: 'mensagem-listar.html'
 })
 export class MensagemListarPage {
-
   mensagens: MensagemDTO[] = new Array<MensagemDTO>();
-
+  mensagensSearch: MensagemDTO[] = new Array<MensagemDTO>();
+  searchQuery: string = '';
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public mensagemService: MensagemService
-    ) {
+    public mensagemService: MensagemService,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController
+  ) {}
+
+  obterLoading() {
+    return this._loadingCtrl.create({
+      content: 'Carregando...'
+    });
   }
 
   ionViewDidLoad() {
-    this.mensagemService.buscaTodos()
-      .subscribe(resposta => {
-        console.log(resposta);
+    let loading = this.obterLoading();
+    loading.present();
+    this.mensagemService.buscaTodos().subscribe(
+      resposta => {
+        loading.dismiss();
         this.mensagens = resposta;
-        
+        console.log(this.mensagens);
+
+        this.mensagensSearch = this.mensagens;
       },
       error => {
-        console.log("error");
+        loading.dismiss();
         console.log(error);
-      });
+        this._alertCtrl
+          .create({
+            title: 'Falha',
+            subTitle: 'Não foi possível obter as Mensagens, tente novamente mais tarde!',
+            buttons: [
+              {
+                text: 'Ok'
+              }
+            ]
+          })
+          .present();
+        this.navCtrl.goToRoot;
+      }
+    );
   }
 
+  copiaListaListaMensagens() {
+    return this.mensagens;
+  }
+  getItems(ev: any) {
+    this.mensagensSearch = this.copiaListaListaMensagens();
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.mensagensSearch = this.mensagensSearch.filter(item => {
+        return (
+          item.autor.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          item.mensagem.toLowerCase().indexOf(val.toLowerCase()) > -1
+        );
+      });
+    }
+  }
 }
