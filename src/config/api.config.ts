4f539f3f6107ffdc3f6c9088eff67eb138dfe28e
Ednezer de Godoi Rezende <erezende@brq.com>
2021-09-21 10:22:27 -0300

    Alterando url da bibliaOnline

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,5 +2,7 @@ export const API_CONFIG = {
   baseUrl: "https://igreja-cristo.herokuapp.com",
   //baseUrl: "http://localhost:8082",
   bucketBaseUrl: "https://igreja-cristo.s3-sa-east-1.amazonaws.com",
-  urlBibliaOnline: "https://bibleapi.co/api",
+  // urlBibliaOnline: "https://bibleapi.co/api",
+  urlBibliaOnline: "https://www.abibliadigital.com.br/api"
+  
 };
